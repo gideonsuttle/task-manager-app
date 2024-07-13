0680125b952c797ad63e4d121621f4178f7ac5bd
Gideon <gideonsuttle777@gmail.com>
2024-07-13 14:16:43 +0530

    updates

---
+++ src/emails/account.js
@@ -0,0 +1,14 @@
+// const mailtrap = require('mailtrap')
+
+// const mailtrapAPIKey = process.env.MAILTRAP_API_KEY
+
+// const client = new mailtrap.MailtrapClient({ token: mailtrapAPIKey });
+
+// const sender = {name: "Test", email: 'gideonsuttle777@gmail.com'}
+
+// client.send({
+//     to: [{ email: 'gideonsuttle@karunya.edu.in'}],
+//     from: sender,
+//     subject: 'First creation',
+//     test: 'Hope you good g'
+// }).then(console.log).catch(console.error)
