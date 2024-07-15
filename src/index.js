5e6ab31b1b32df226e5a4979fa179e69a8af3d81
Gideon <gideonsuttle777@gmail.com>
2024-07-15 22:40:57 +0530

    changes to vercel file

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
+app.post('/', (req, res) => {
+    res.send('Welcome to the task manager application!')
+})
+
 app.listen(port, () => {
     console.log('Server is up on port ' + port )
 })
+
+// MONGODB_URL=mongodb://127.0.0.1:27017/task-manager-api
